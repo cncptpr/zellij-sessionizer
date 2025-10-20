634f174f33d2da734fa68b29b4d80364dc517878
cncptpr <concept1928@gmail.com>
2025-10-20 11:48:04 +0200

    [lua] Inlined error handeling

--- lua/zellij-sessionizer.lua
+++ lua/zellij-sessionizer.lua
@@ -3,12 +3,6 @@
 local lfs = require("lfs")
 
 ---@alias path string Path to a file
----@alias error_type
----| "NESTED_ZELLIJ_NOT_ALLOWED"
----| "NO_PATH_SPECIFIED"
----| "DIR_NOT_FOUND"
----| "NO_VALIDE_DIRS"
----| "FZF_EXECUTION_FAILED"
 
 local ANSI = {
   RESET = "\x1B[0m",
@@ -16,33 +10,6 @@ local ANSI = {
   GREEN = "\x1B[32m",
   YELLOW = "\x1B[33m",
 }
-
----Prints error message of provided type
----This function feels horrible, but I wanted to try out the enum type alias :)
----@param type error_type
----@param ctx path?
-local function error(type, ctx)
-  if type == "NO_PATH_SPECIFIED" then
-    print "No paths were specified, usage: ./zellij-sessionizer path1 path2 etc.."
-  elseif type == "NESTED_ZELLIJ_NOT_ALLOWED" then
-    print(ANSI.RED .. "Zellij environment detected!" .. ANSI.RESET)
-    print("Script only works outside of Zellij.")
-    print("")
-    print("This is because nested Zellij sessions are not recommended,")
-    print("and it is currently not possible to change Zellij sessions")
-    print("from within a script.")
-    print("")
-    print("Unset " .. ANSI.GREEN .. "ZELLIJ" .. ANSI.RESET .. " env var to force this script to work.")
-  elseif type == "DIR_NOT_FOUND" then
-    print(ANSI.YELLOW .. "Warning:" .. ANSI.RESET .. " Directory not found: " .. ctx)
-    return -- no error, just warning
-  elseif type == "NO_VALIDE_DIRS" then
-    print("No valid directories found to choose from.")
-  elseif type == "FZF_EXECUTION_FAILED" then
-    print(ANSI.RED .. "Error:" .. ANSI.RESET .. " Failed to execute fzf")
-  end
-  os.exit(1)
-end
 
 ---Checks wether path is a dir
 ---@param path path
@@ -77,7 +44,7 @@ local function append_all_paths(list, path)
 
   local base_path = path:sub(1, -3) -- Remove the '/*' suffix
   if not is_dir(base_path) then
-    error("DIR_NOT_FOUND", base_path)
+    print(ANSI.YELLOW .. "Warning:" .. ANSI.RESET .. " Directory not found: " .. base_path)
     return false
   end
 
@@ -89,11 +56,15 @@ end
 
 ---Uses fzf cli to promt user with entries from list
 ---@param list table<path>
----@return path selected entry
+---@return path|nil selected entry
 local function fzf(list)
   local fzf_input = table.concat(list, "\n")
   local fzf_handle = io.popen("printf '%s\\n' '" .. fzf_input .. "' | fzf")
-      or error("FZF_EXECUTION_FAILED")
+  if not fzf_handle then
+     print(ANSI.RED .. "Error:" .. ANSI.RESET .. " Failed to execute fzf")
+     return nil
+  end
+
   local selected_path = fzf_handle:read("*l")
   fzf_handle:close()
   return selected_path
@@ -101,36 +72,48 @@ end
 
 local function main(paths)
   if os.getenv("ZELLIJ") ~= nil then
-    error "NESTED_ZELLIJ_NOT_ALLOWED"
+    print(ANSI.RED .. "Zellij environment detected!" .. ANSI.RESET)
+    print("Script only works outside of Zellij.")
+    print("")
+    print("This is because nested Zellij sessions are not recommended,")
+    print("and it is currently not possible to change Zellij sessions")
+    print("from within a script.")
+    print("")
+    print("Unset " .. ANSI.GREEN .. "ZELLIJ" .. ANSI.RESET .. " env var to force this script to work.")
+    return 1
   end
 
   if #paths == 0 then
-    error "NO_PATH_SPECIFIED"
+    print "No paths were specified, usage: ./zellij-sessionizer path1 path2 etc.."
+    return 1
   end
 
   -- Collect all requested dirs and subdirs
   local candidates = {}
   for _, path in ipairs(paths) do
     if not append_all_paths(candidates, path) then
-      error("DIR_NOT_FOUND", path)
+      print(ANSI.YELLOW .. "Warning:" .. ANSI.RESET .. " Directory not found: " .. path)
     end
   end
 
   if #candidates == 0 then
-    error "NO_VALIDE_DIRS"
+    print("No valid directories found to choose from.")
+
   end
 
   local selected_path = fzf(candidates)
   if not selected_path or selected_path == "" then
     -- If nothing was picked, silently exit
-    os.exit(0)
+    return 0
   end
 
   -- replacing "." in path with "_" for the session name
   local session_name = selected_path:match("([^/]+)$"):gsub("%.", "_")
 
   os.execute("cd " .. selected_path .. " && zellij attach " .. session_name .. " -c")
-  os.exit(0)
+  return 0
 end
 
-main({ ... })
+
+local code = main({ ... })
+os.exit(code)
