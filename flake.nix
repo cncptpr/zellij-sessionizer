c7d36e83f7e0fc13d331030c4935a0ed67cb21c1
cncptpr <concept1928@gmail.com>
2025-11-24 16:50:11 +0100

    C works!!!

--- flake.nix
+++ flake.nix
@@ -2,32 +2,36 @@
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = {self, nixpkgs, ...}:
-  let
+  outputs = {self, nixpkgs, ...}: let
     pname = "zellij-sessionizer";
     forAllSystems = cb:
       nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
       ] (system: cb {pkgs = nixpkgs.legacyPackages.${system}; system = system;});
-  in
-  {
-    packages = forAllSystems ({pkgs, system}: {
+  in {
+    packages = forAllSystems ({pkgs, system}:
+    {
       default = self.packages.${system}.c;
-      c = pkgs.stdenv.mkDerivation {
-        pname = pname;
-        version = "0.1";
-        src=./c;
-        buildInputs = with pkgs; [fzf zellij];
-        buildPhase = ''
-          gcc nob.c -o nob
-          ./nob
-        '';
-        installPhase = ''
-          mkdir -p $out/bin
-          cp zellij-sessionizer $out/bin
-        '';
-      };
+      c = let
+          build = pkgs.stdenv.mkDerivation {
+            pname = pname;
+            version = "0.1";
+            src=./c;
+            nativeBuildInputs = with pkgs; [makeWrapper];
+            buildPhase = ''
+              gcc nob.c -o nob
+              ./nob
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp zellij-sessionizer $out/bin/zellij-sessionizer
+              wrapProgram $out/bin/zellij-sessionizer \
+              --prefix PATH : ${pkgs.fzf}/bin  \
+              --prefix PATH : ${pkgs.zellij}/bin
+            '';
+          };
+        in build;
       bash = pkgs.writeShellApplication {
         name = pname;
         text = builtins.readFile ./zellij-sessionizer.sh ;
@@ -35,12 +39,9 @@
       };
     });
 
-    devShells = forAllSystems (pkgs: {
+    devShells = forAllSystems ({pkgs, system}: {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          ocaml
-          opam
-        ];
+        packages = with pkgs; [ fzf zellij ];
         shellHook = ''
           eval (opam env)
         '';
